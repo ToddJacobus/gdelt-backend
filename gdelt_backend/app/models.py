91afa9ecb10f2d971981ad7f1a269d2c499f1e2a
Todd Jacobus <tj@tcarta.com>
2019-10-17 06:46:23 -0600

    installed architect; creating models...

--- gdelt_backend/app/models.py
+++ gdelt_backend/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import architect
 
 # Create your models here.
+class Sources(models.Model):
+    gkg_id = models.CharField(max_length=50, unique=True)
+    pub_date = models.DateField()
+    pub_id = models.IntegerField()
+    source_name = models.CharField(max_length=100)
+    doc_uri = models.TextField()
+    tone = models.FloatField()
+    positive_score = models.FloatField()
+    negative_score = models.FloatField()
+    polarity = models.FloatField()
+    activity_ref_density = models.FloatField()
+    pronoun_ref_density = models.FloatField()
+    word_count = models.FloatField()
+    share_image = models.TextField()
+
+
+class Counts(modesl.Model):
+    source_id = models.Integer
+    # TODO: finish me....
